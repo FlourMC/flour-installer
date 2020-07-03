5cdfe57a8a210dd135ba3c6197c5e6660a3f9cfe
Enaium <32991121+Enaium@users.noreply.github.com>
2020-07-03 22:38:07 +0800

    init

---
+++ src/main/java/cn/enaium/flourmc/installer/FlourInstaller.java
@@ -0,0 +1,12 @@
+package cn.enaium.flourmc.installer;
+
+/**
+ * Project: flour-installer
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+public class FlourInstaller {
+    public static void main(String[] args) {
+
+    }
+}
